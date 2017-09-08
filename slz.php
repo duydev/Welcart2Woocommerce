efbfd6ef27473524f15c4984db65db1aee669ddc
Trần Nhật Duy <trannhatduy297@gmail.com>
2017-09-08 14:56:41 +0700

    update

--- slz.php
+++ slz.php
@@ -5,9 +5,10 @@ require_once './wp-load.php';
 $s = new Welcart_Export();
 $d = new Woocommerce_Export();
 
-echo $s->export_tags_to_string();
-
-//$d->import_category_from_string( $cat_json );
+$d->clean_terms( $d->get_category_taxonomy() );
+$d->clean_terms( $d->get_tag_taxonomy() );
+$d->import_terms_from_string( $s->export_category_to_string() );
+$d->import_terms_from_string( $s->export_tag_to_string() );
 
 
 class Welcart_Export {
@@ -24,75 +25,76 @@ class Welcart_Export {
 		return defined( 'USCES_ITEM_CAT_PARENT_ID' ) ? USCES_ITEM_CAT_PARENT_ID : 0;
 	}
 
-	public function export_category_to_string() {
-		$output = array();
-		$this->_export_category( $this->get_root_term_id(), $output );
-
-		return json_encode( $output, JSON_PRETTY_PRINT );
-	}
-
-	public function _export_category( $term_id, &$output = array(), $parent_slug = '' ) {
-		$term = get_term( $term_id, $this->get_category_taxonomy() );
+	public function _export_terms( $term_id, $taxonomy, &$output = array(), $parent_slug = '' ) {
+		$term_slug = '';
+		$term      = get_term( $term_id, $taxonomy );
 		if ( $term && ! is_wp_error( $term ) ) {
-			$item     = array(
+			$term_slug = $term->slug;
+			$item      = array(
+				'taxonomy'    => $taxonomy,
 				'name'        => $term->name,
 				'slug'        => $term->slug,
 				'description' => $term->description,
 				'parent_slug' => $parent_slug,
 			);
-			$output[] = $item;
+			$output[]  = $item;
+		}
 
-			$metas = get_term_meta( $term->term_id );
-			var_dump( $metas );
-
-			$child_terms = get_terms( array(
-				'taxonomy'   => $this->get_category_taxonomy(),
-				'hide_empty' => true,
-				'parent'     => $term->term_id,
-			) );
-			if ( $child_terms && ! is_wp_error( $child_terms ) ) {
-				foreach ( $child_terms as $child_term ) {
-					$this->_export_category( $child_term->term_id, $output, $term->slug );
-				}
+		$child_terms = get_terms( array(
+			'taxonomy'   => $taxonomy,
+			'hide_empty' => true,
+			'parent'     => $term_id,
+		) );
+		if ( $child_terms && ! is_wp_error( $child_terms ) ) {
+			foreach ( $child_terms as $child_term ) {
+				$this->_export_terms( $child_term->term_id, $taxonomy, $output, $term_slug );
 			}
 		}
 	}
 
-	public function export_tags_to_string() {
-		$tags = get_tags( array(
-			'taxonomy' => $this->get_tag_taxonomy(),
-			'hide_empty' => false,
-		) );
+	public function export_category_to_string() {
+		$output = array();
+		$this->_export_terms( $this->get_root_term_id(), $this->get_category_taxonomy(), $output );
 
-		var_dump( $tags );
+		return json_encode( $output, JSON_PRETTY_PRINT );
+	}
 
+	public function export_tag_to_string() {
+		$output = array();
+		$this->_export_terms( 0, $this->get_tag_taxonomy(), $output );
+
+		return json_encode( $output, JSON_PRETTY_PRINT );
 	}
 
 }
 
 class Woocommerce_Export {
 
-	public function get_taxonomy() {
+	public function get_category_taxonomy() {
 		return 'product_cat';
 	}
 
-	public function import_category_from_string( $content ) {
+	public function get_tag_taxonomy() {
+		return 'product_tag';
+	}
+
+	public function import_terms_from_string( $content ) {
 		$items = json_decode( $content, true );
 		if ( is_array( $items ) ) {
 			foreach ( $items as $item ) {
-				wp_insert_term( $item['name'], $this->get_taxonomy(), array(
+				$taxonomy = $item['taxonomy'] == 'category' ? $this->get_category_taxonomy() : $this->get_tag_taxonomy();
+				wp_insert_term( $item['name'], $taxonomy, array(
 					'slug'        => $item['slug'],
 					'description' => $item['description'],
-					'parent'      => $this->term_slug_to_term_id( $item['parent_slug'] ),
+					'parent'      => $this->term_slug_to_term_id( $item['parent_slug'], $taxonomy ),
 				) );
 			}
 		}
 	}
 
-	public function term_slug_to_term_id( $term_slug ) {
-		$id = 0;
-
-		$term = get_term_by( 'slug', $term_slug, $this->get_taxonomy() );
+	public function term_slug_to_term_id( $term_slug, $taxonomy ) {
+		$id   = 0;
+		$term = get_term_by( 'slug', $term_slug, $taxonomy );
 		if ( $term && ! is_wp_error( $term ) ) {
 			$id = $term->term_id;
 		}
@@ -100,14 +102,14 @@ class Woocommerce_Export {
 		return $id;
 	}
 
-	public function clean_categories() {
+	public function clean_terms( $taxonomy ) {
 		$terms = get_terms( array(
-			'taxonomy'   => $this->get_taxonomy(),
+			'taxonomy'   => $taxonomy,
 			'hide_empty' => false,
 		) );
 		if ( $terms && ! is_wp_error( $terms ) ) {
 			foreach ( $terms as $term ) {
-				wp_delete_term( $term->term_id, WC_TAXONOMY );
+				wp_delete_term( $term->term_id, $taxonomy );
 			}
 		}
 	}
